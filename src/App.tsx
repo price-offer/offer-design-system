061beb97e8a9d9f1883c4ac03fa1b5f7f6b66188
sonsurim <surim014@naver.com>
2022-08-29 01:51:28 +0900

    config: eslint, prettier, alias 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import type { ReactElement } from 'react'
+import reactLogo from './assets/react.svg'
+import { useState } from 'react'
 
-function App() {
+const App = (): ReactElement => {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
+          <img alt="Vite logo" className="logo" src="/vite.svg" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" rel="noreferrer" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          type="button"
+          onClick={(): void => {
+            setCount(count => count + 1)
+          }}>
           count is {count}
         </button>
         <p>

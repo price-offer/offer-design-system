a99e00d64a1110a8af5825cf18d4ab104a878bfe
chojooyoung <jzo09@naver.com>
2022-08-30 00:44:32 +0900

    refactor: 자동배포테스트 커밋

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const Test = () => {
-  return <div>배포테스트용 컴포넌트</div>
+  return <div>배포테스트용 컴포넌트입니둥~</div>
 }
 
 export default Test

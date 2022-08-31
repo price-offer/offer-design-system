02f5bd9fe5b80322cd8c1451ad7c69c0a564b2f7
sonsurim <surim014@naver.com>
2022-09-01 01:15:13 +0900

    feat: global style 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import App from './App'
+import { GlobalStyle } from '@styles'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { theme } from '@themes'
@@ -7,6 +8,7 @@ import { ThemeProvider } from '@emotion/react'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>

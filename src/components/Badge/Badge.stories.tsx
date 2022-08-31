1ff94516c1415a8a0709ce0c70e47420265a1c90
sonsurim <surim014@naver.com>
2022-08-31 23:07:11 +0900

    feat: Badge Component stories argType 추가

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -2,7 +2,12 @@ import type { ComponentMeta, ComponentStory } from '@storybook/react'
 import { Badge } from './index'
 
 export default {
-  argTypes: {},
+  argTypes: {
+    colorScheme: {
+      control: { type: 'radio' },
+      options: ['gray', 'orange', 'purple']
+    }
+  },
   component: Badge,
   title: 'Component/Badge'
 } as ComponentMeta<typeof Badge>

9a5a567ec76de70a4aa5b7d1e910fe9688c71630
shinhyojeong <hyojeonge0@naver.com>
2022-09-01 01:24:46 +0900

    💚 chore: radius 속성 추가

--- src/themes/radius.ts
+++ src/themes/radius.ts
@@ -6,8 +6,9 @@ type RadiusValues = typeof radius[RadiusKeys]
 export type Radius = ThemeOption<RadiusKeys, RadiusValues>
 
 export const radius = {
-  round04: '4px',
-  round08: '8px',
+  round100: '100px',
   round12: '12px',
-  round16: '16px'
+  round16: '16px',
+  round4: '4px',
+  round8: '8px'
 } as const

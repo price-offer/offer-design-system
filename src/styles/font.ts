9f002270ec1b9a0fa313c9364d3458d3eb27d10d
Sonny <47546413+sonsurim@users.noreply.github.com>
2022-09-01 10:58:51 +0900

    feat: reset css, global font face 적용, 기본 구조 제거 (#13)
    * assets: font 파일 추가
    
    * chore: 불필요한 기본 파일 제거
    
    * style: theme 디렉토리 styles 내부로 이동
    
    * feat: font face, reset css 생성
    
    * feat: global style 적용
    
    * feat: storybook global style 반영
    
    * Squashed commit of the following:
    - chore: radius 속성 추가 (#14)
    
    * Squashed commit of the following:
    - chore: radius 속성 추가 (#14)
    - feat: storybook global style 반영
    - feat: global style 적용
    - feat: font face, reset css 생성
    - style: theme 디렉토리 styles 내부로 이동
    - chore: 불필요한 기본 파일 제거
    - assets: font 파일 추가

---
+++ src/styles/font.ts
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react'
+
+export const FontCSS = css`
+  @font-face {
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: regular;
+    src: url('../../public/fonts/Pretendard-Regular.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: medium;
+    src: url('../../public/fonts/Pretendard-Medium.woff') format('woff');
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: bold;
+    src: url('../../public/fonts/Pretendard-Bold.woff') format('woff');
+  }
+`

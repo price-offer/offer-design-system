c9f71bbb2e4d2313f4edbf5f07df550a407294b3
sonsurim <surim014@naver.com>
2022-09-01 23:30:48 +0900

    refactor: font root에서 참조하도록 변경

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -5,18 +5,18 @@ export const FontCSS = css`
     font-family: 'Pretendard';
     font-style: normal;
     font-weight: regular;
-    src: url('../../public/fonts/Pretendard-Regular.woff') format('woff');
+    src: url('/fonts/Pretendard-Regular.woff') format('woff');
   }
   @font-face {
     font-family: 'Pretendard';
     font-style: normal;
     font-weight: medium;
-    src: url('../../public/fonts/Pretendard-Medium.woff') format('woff');
+    src: url('/fonts/Pretendard-Medium.woff') format('woff');
   }
   @font-face {
     font-family: 'Pretendard';
     font-style: normal;
     font-weight: bold;
-    src: url('../../public/fonts/Pretendard-Bold.woff') format('woff');
+    src: url('/fonts/Pretendard-Bold.woff') format('woff');
   }
 `

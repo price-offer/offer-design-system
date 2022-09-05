e6fa0328fcbd502f3d4603173cf9d1717b1121ae
shinhyojeong <hyojeonge0@naver.com>
2022-09-05 17:37:37 +0900

    ✨ feat: theme font 구조 변경

--- src/styles/themes/fonts.ts
+++ src/styles/themes/fonts.ts
@@ -1,47 +1,34 @@
 import type { ThemeOption } from '@types'
 
-type FontStyleKeys = 'bold' | 'medium' | 'regular' | 'long'
-type FontStyle = ThemeOption<FontStyleKeys, string>
+type FontStyleKeys = keyof typeof fonts
+type FontStyleValues = typeof fonts[FontStyleKeys]
 
-export interface Fonts {
-  body01: Omit<FontStyle, 'long'>
-  body02: FontStyle
-  caption: string
-  display01: string
-  display02: Pick<FontStyle, 'bold' | 'medium'>
-  headline01: string
-  headline02: string
-  subtitle01: Pick<FontStyle, 'bold' | 'medium'>
-}
+export type Fonts = ThemeOption<FontStyleKeys, FontStyleValues>
 
-export const fonts: Fonts = {
-  body01: {
-    bold: 'font-size: 16px; font-weight: bold; line-height: 24px; letter-spacing: -0.6%;',
-    medium:
-      'font-size: 16px; font-weight: 500; line-height: 24px; letter-spacing: -0.6%;',
-    regular:
-      'font-size: 16px; font-weight: regular; line-height: 24px; letter-spacing: -0.6%;'
-  },
-  body02: {
-    bold: 'font-size: 14px; font-weight: bold; line-height: 24px; letter-spacing: -0.6%;',
-    long: 'font-size: 14px; font-weight: regular; line-height: 26px; letter-spacing: -0.4%;',
-    medium: 'font-size: 14px; font-weight: 500; line-height: 20px;',
-    regular:
-      'font-size: 14px; font-weight: regular; line-height: 14px; letter-spacing: -0.4%;'
-  },
+export const fonts = {
+  body01B:
+    'font-size: 16px; font-weight: bold; line-height: 24px; letter-spacing: -0.6%;',
+  body01M:
+    'font-size: 16px; font-weight: 500; line-height: 24px; letter-spacing: -0.6%;',
+  body01R:
+    'font-size: 16px; font-weight: regular; line-height: 24px; letter-spacing: -0.6%;',
+  body02B:
+    'font-size: 14px; font-weight: bold; line-height: 24px; letter-spacing: -0.6%;',
+  body02L:
+    'font-size: 14px; font-weight: regular; line-height: 26px; letter-spacing: -0.4%;',
+  body02M: 'font-size: 14px; font-weight: 500; line-height: 20px;',
+  body02R:
+    'font-size: 14px; font-weight: regular; line-height: 14px; letter-spacing: -0.4%;',
   caption:
     'font-size: 12px; font-weight: bold; line-height: 16px; letter-spacing: -0.3%;',
   display01:
     'font-size: 36px; font-weight: bold; line-height: 40px; letter-spacing: -0.6%;',
-  display02: {
-    bold: 'font-size: 28px; font-weight: bold; line-height: 28px;',
-    medium: 'font-size: 28px; font-weight: 500; line-height: 28px;'
-  },
+  display02B: 'font-size: 28px; font-weight: bold; line-height: 28px;',
+  display02M: 'font-size: 28px; font-weight: 500; line-height: 28px;',
   headline01: 'font-size: 24px; font-weight: bold; line-height: 32px;',
   headline02: 'font-size: 20px; font-weight: bold; line-height: 28px;',
-  subtitle01: {
-    bold: 'font-size: 18px; font-weight: bold; line-height: 22px; letter-spacing: -0.6%;',
-    medium:
-      'font-size: 18px; font-weight: 500; line-height: 22px; letter-spacing: -0.4%;'
-  }
+  subtitle01B:
+    'font-size: 18px; font-weight: bold; line-height: 22px; letter-spacing: -0.6%;',
+  subtitle01M:
+    'font-size: 18px; font-weight: 500; line-height: 22px; letter-spacing: -0.4%;'
 } as const

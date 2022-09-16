fce14c1d96b4b4ac280314db389af091901e65af
shinhyojeong <hyojeonge0@naver.com>
2022-09-17 04:52:19 +0900

    feat: useClickAway hook 추가

---
+++ src/hooks/useClickAway.ts
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from 'react'
+import type { RefObject } from 'react'
+
+const events = ['mousedown', 'touchstart']
+
+export const useClickAway = <T extends HTMLElement>(
+  handler: (e?: Event) => void
+): RefObject<T> => {
+  const ref = useRef<T>(null)
+  const savedHandler = useRef(handler)
+
+  useEffect(() => {
+    savedHandler.current = handler
+  }, [handler])
+
+  useEffect(() => {
+    const element = ref.current
+    if (!element) {
+      return
+    }
+
+    const handleEvent: EventListener = e => {
+      !element.contains(e.target as HTMLElement) && savedHandler.current(e)
+    }
+
+    for (const eventName of events) {
+      document.addEventListener(eventName, handleEvent)
+    }
+
+    return () => {
+      for (const eventName of events) {
+        document.removeEventListener(eventName, handleEvent)
+      }
+    }
+  }, [ref])
+
+  return ref
+}

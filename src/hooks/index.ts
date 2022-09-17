598ae6c780d016ec2c288dcd651207e2c8de6ae1
sonsurim <surim014@naver.com>
2022-09-17 17:55:16 +0900

    feat: useUploader 훅 구현

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from './useMediaQuery'
 export * from './useImage'
+export * from './useUploader'
---
+++ src/hooks/useUploader.ts
@@ -0,0 +1,103 @@
+import type {
+  ChangeEventHandler,
+  MouseEventHandler,
+  MutableRefObject
+} from 'react'
+import type { File, OnChangeParams } from '@components'
+import { useRef, useState } from 'react'
+import { v4 as uuidV4 } from 'uuid'
+
+interface UseUploaderParams {
+  fileList: File[]
+  onChange(params: OnChangeParams): void
+}
+
+interface UseUploaderReturn {
+  files: File[]
+  uploaderRef: MutableRefObject<HTMLInputElement | null>
+  fileListRef: MutableRefObject<HTMLDivElement | null>
+  uploaderId: string
+  addFile: ChangeEventHandler<HTMLInputElement>
+  removeFile: MouseEventHandler<HTMLDivElement>
+  clickTrigger: MouseEventHandler<HTMLDivElement>
+}
+
+type UseUploader = (params: UseUploaderParams) => UseUploaderReturn
+
+export const useUploader: UseUploader = ({ fileList, onChange }) => {
+  const uploaderRef = useRef<HTMLInputElement | null>(null)
+  const fileListRef = useRef<HTMLDivElement | null>(null)
+  const [files, setFiles] = useState<File[]>(fileList)
+  const uploaderId = uuidV4()
+
+  const clickTrigger: MouseEventHandler<HTMLDivElement> = () => {
+    if (files.length === 10) {
+      alert('사진은 최대 10장만 추가가 가능합니다.')
+      return
+    }
+
+    uploaderRef.current?.click()
+  }
+
+  const removeFile: MouseEventHandler<HTMLDivElement> = (e): void => {
+    if (!fileListRef.current) {
+      return
+    }
+
+    const closeIcons = fileListRef.current.querySelectorAll(
+      '[data-id="close-icon"]'
+    )
+    const fileIndex = Array.from(closeIcons).findIndex(
+      icon => icon === e.target
+    )
+
+    const firstToIndexFileList = files.slice(0, fileIndex)
+    const indexToLastFileList = files.slice(fileIndex + 1)
+
+    const isRepresent = files[fileIndex].isRepresent && files.length > 1
+    if (isRepresent) {
+      firstToIndexFileList[0].isRepresent = true
+    }
+
+    const newFiles = [...firstToIndexFileList, ...indexToLastFileList]
+    onChange({ eventType: 'remove', fileList: newFiles })
+    setFiles(newFiles)
+  }
+
+  const addFile: ChangeEventHandler<HTMLInputElement> = e => {
+    const formData = new FormData()
+
+    if (!e.target.files) {
+      return
+    }
+
+    const imageFile = e.target.files[0]
+    formData.append('image', imageFile)
+
+    const reader = new FileReader()
+    reader.readAsDataURL(imageFile)
+    reader.onload = (): void => {
+      const isFirst = files.length === 0
+      const newFile = {
+        id: uuidV4(),
+        isRepresent: isFirst || false,
+        name: 'new!',
+        url: `${reader.result}`
+      }
+
+      const newFiles = [...files, newFile]
+      onChange({ eventType: 'upload', fileList: newFiles })
+      setFiles(newFiles)
+    }
+  }
+
+  return {
+    addFile,
+    clickTrigger,
+    fileListRef,
+    files,
+    removeFile,
+    uploaderId,
+    uploaderRef
+  }
+}

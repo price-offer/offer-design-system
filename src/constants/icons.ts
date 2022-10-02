b35f93b326f0d99cc5808fbde511c2c74b0692e2
shinhyojeong <hyojeonge0@naver.com>
2022-10-02 22:47:15 +0900

    chore: 중복되는 svg 파일 제거

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,105 +1,69 @@
-import ARROW_LEFT_24 from '/public/icons/arrow_left_24.svg'
-import ARROW_UP_24 from '/public/icons/arrow_up_24.svg'
+import ARROW_LEFT from '/public/icons/arrow_left.svg'
+import ARROW_UP from '/public/icons/arrow_up.svg'
 import AVATAR from '/public/icons/avatar.svg'
-import BOX_16 from '/public/icons/box_16.svg'
-import CAMERA_16 from '/public/icons/camera_16.svg'
-import CAMERA_24 from '/public/icons/camera_24.svg'
-import CHECK_CIRCLE_16 from '/public/icons/check_circle_16.svg'
-import CHECK_CIRCLE_FILL_100 from '/public/icons/check_circle_fill_100.svg'
-import CHEVRON_DOWN_16 from '/public/icons/chevron_down_16.svg'
-import CHEVRON_LEFT_16 from '/public/icons/chevron_left_16.svg'
-import CHEVRON_LEFT_40 from '/public/icons/chevron_left_40.svg'
-import CHEVRON_RIGHT_14 from '/public/icons/chevron_right_14.svg'
-import CHEVRON_RIGHT_16 from '/public/icons/chevron_right_16.svg'
-import CHEVRON_RIGHT_40 from '/public/icons/chevron_right_40.svg'
-import CLOCK_16 from '/public/icons/clock_16.svg'
-import CLOCK_24 from '/public/icons/clock_24.svg'
-import CLOSE_16 from '/public/icons/close_16.svg'
-import CLOSE_24 from '/public/icons/close_24.svg'
-import FILTER_16 from '/public/icons/filter_16.svg'
-import FILTER_24 from '/public/icons/filter_24.svg'
-import GOOGLE_24 from '/public/icons/google_24.svg'
-import HEART_16 from '/public/icons/heart_16.svg'
-import HEART_24 from '/public/icons/heart_24.svg'
-import HEART_ACTIVE_16 from '/public/icons/heart_active_16.svg'
-import HEART_ACTIVE_24 from '/public/icons/heart_active_24.svg'
-import KAKAO_24 from '/public/icons/kakao_24.svg'
-import MEH_40 from '/public/icons/meh_40.svg'
-import MEH_ACTIVE_24 from '/public/icons/meh_active_24.svg'
-import MEH_ACTIVE_40 from '/public/icons/meh_active_40.svg'
-import MENU_24 from '/public/icons/menu_24.svg'
-import MESSAGE_100 from '/public/icons/message_100.svg'
-import MESSAGE_16 from '/public/icons/message_16.svg'
-import MESSAGE_24 from '/public/icons/message_24.svg'
-import MORE_16 from '/public/icons/more_16.svg'
-import MORE_24 from '/public/icons/more_24.svg'
-import PICTURE_40 from '/public/icons/picture_40.svg'
-import REFRESH_16 from '/public/icons/refresh_16.svg'
-import REFRESH_24 from '/public/icons/refresh_24.svg'
-import SAD_40 from '/public/icons/sad_40.svg'
-import SAD_ACTIVE_24 from '/public/icons/sad_active_24.svg'
-import SAD_ACTIVE_40 from '/public/icons/sad_active_40.svg'
-import SEARCH_24 from '/public/icons/search_24.svg'
-import SETTING_24 from '/public/icons/setting_24.svg'
-import SMILE_16 from '/public/icons/smile_16.svg'
-import SMILE_24 from '/public/icons/smile_24.svg'
-import SMILE_40 from '/public/icons/smile_40.svg'
-import SMILE_ACTIVE_24 from '/public/icons/smile_active_24.svg'
-import SMILE_ACTIVE_40 from '/public/icons/smile_active_40.svg'
-import STORE_16 from '/public/icons/store_16.svg'
-import STORE_24 from '/public/icons/store_24.svg'
-import TRIANGLE_DOWN_16 from '/public/icons/triangle_down_16.svg'
+import BOX from '/public/icons/box.svg'
+import CAMERA from '/public/icons/camera.svg'
+import CHECK from '/public/icons/check.svg'
+import CHECK_CIRCLE from '/public/icons/check_circle.svg'
+import CHECK_CIRCLE_FILL from '/public/icons/check_circle_fill.svg'
+import CHEVRON_DOWN from '/public/icons/chevron_down.svg'
+import CHEVRON_LEFT from '/public/icons/chevron_left.svg'
+import CHEVRON_RIGHT from '/public/icons/chevron_right.svg'
+import CLOCK from '/public/icons/clock.svg'
+import CLOSE from '/public/icons/close.svg'
+import FILTER from '/public/icons/filter.svg'
+import GOOGLE from '/public/icons/google.svg'
+import HEART from '/public/icons/heart.svg'
+import HEART_FILL from '/public/icons/heart_fill.svg'
+import KAKAO from '/public/icons/kakao.svg'
+import MEH from '/public/icons/meh.svg'
+import MEH_FILL from '/public/icons/meh_fill.svg'
+import MENU from '/public/icons/menu.svg'
+import MESSAGE from '/public/icons/message.svg'
+import MORE from '/public/icons/more.svg'
+import PICTURE from '/public/icons/picture.svg'
+import REFRESH from '/public/icons/refresh.svg'
+import SAD from '/public/icons/sad.svg'
+import SAD_FILL from '/public/icons/sad_fill.svg'
+import SEARCH from '/public/icons/search.svg'
+import SETTING from '/public/icons/setting.svg'
+import SMILE from '/public/icons/smile.svg'
+import SMILE_FILL from '/public/icons/smile_fill.svg'
+import STORE from '/public/icons/store.svg'
+import TRIANGLE_DOWN from '/public/icons/triangle_down.svg'
 
 export const ICON = {
-  ARROW_LEFT_24,
-  ARROW_UP_24,
+  ARROW_LEFT,
+  ARROW_UP,
   AVATAR,
-  BOX_16,
-  CAMERA_16,
-  CAMERA_24,
-  CHECK_CIRCLE_16,
-  CHECK_CIRCLE_FILL_100,
-  CHEVRON_DOWN_16,
-  CHEVRON_LEFT_16,
-  CHEVRON_LEFT_40,
-  CHEVRON_RIGHT_14,
-  CHEVRON_RIGHT_16,
-  CHEVRON_RIGHT_40,
-  CLOCK_16,
-  CLOCK_24,
-  CLOSE_16,
-  CLOSE_24,
-  FILTER_16,
-  FILTER_24,
-  GOOGLE_24,
-  HEART_16,
-  HEART_24,
-  HEART_ACTIVE_16,
-  HEART_ACTIVE_24,
-  KAKAO_24,
-  MEH_40,
-  MEH_ACTIVE_24,
-  MEH_ACTIVE_40,
-  MENU_24,
-  MESSAGE_100,
-  MESSAGE_16,
-  MESSAGE_24,
-  MORE_16,
-  MORE_24,
-  PICTURE_40,
-  REFRESH_16,
-  REFRESH_24,
-  SAD_40,
-  SAD_ACTIVE_24,
-  SAD_ACTIVE_40,
-  SEARCH_24,
-  SETTING_24,
-  SMILE_16,
-  SMILE_24,
-  SMILE_40,
-  SMILE_ACTIVE_24,
-  SMILE_ACTIVE_40,
-  STORE_16,
-  STORE_24,
-  TRIANGLE_DOWN_16
+  BOX,
+  CAMERA,
+  CHECK,
+  CHECK_CIRCLE,
+  CHECK_CIRCLE_FILL,
+  CHEVRON_DOWN,
+  CHEVRON_LEFT,
+  CHEVRON_RIGHT,
+  CLOCK,
+  CLOSE,
+  FILTER,
+  GOOGLE,
+  HEART,
+  HEART_FILL,
+  KAKAO,
+  MEH,
+  MEH_FILL,
+  MENU,
+  MESSAGE,
+  MORE,
+  PICTURE,
+  REFRESH,
+  SAD,
+  SAD_FILL,
+  SEARCH,
+  SETTING,
+  SMILE,
+  SMILE_FILL,
+  STORE,
+  TRIANGLE_DOWN
 }

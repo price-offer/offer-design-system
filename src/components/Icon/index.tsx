6d5f809bbd374fb608e5bb1ca5b4f9fd7acc5a3e
shinhyojeong <hyojeonge0@naver.com>
2022-10-07 02:27:46 +0900

    refactor: Icon 컴포넌트 props 네이밍 변경

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -41,7 +41,7 @@ export type IconType = keyof typeof ICON_TYPES
 export interface IconProps extends HTMLAttributes<HTMLOrSVGElement> {
   size?: number
   color?: string
-  iconType: IconType
+  type: IconType
 }
 
 type StyledIconWrapperProps = StyledProps<IconProps, 'color'>
@@ -82,12 +82,12 @@ export const ICON_TYPES = {
 } as const
 
 export const Icon = ({
-  iconType,
+  type,
   size = 24,
   color = 'black',
   ...props
 }: IconProps): ReactElement => {
-  const IconSvg = ICON_TYPES[iconType]
+  const IconSvg = ICON_TYPES[type]
 
   return (
     <StyledIconWrapper color={color}>

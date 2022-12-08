7d944741d30c601da5b98bc151c5caaae6b7fc56
sonsurim <surim014@naver.com>
2022-12-09 01:42:11 +0900

    remove unused emotion cache

--- packages/react/src/styles/createEmotionCache.ts
+++
@@ -1,6 +0,0 @@
-import createCache from '@emotion/cache'
-import type { EmotionCache } from '@emotion/react'
-
-export const createEmotionCache = (): EmotionCache => {
-  return createCache({ key: 'css', prepend: true })
-}

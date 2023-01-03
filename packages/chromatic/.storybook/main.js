d61d079bfb5a749ec456c6dee6c7d3df46fae8fc
sonsurim <surim014@naver.com>
2023-01-03 22:51:52 +0900

    fix chromatic storybook alias error

--- packages/chromatic/.storybook/main.js
+++ packages/chromatic/.storybook/main.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const tsconfigPaths = require("vite-tsconfig-paths");
 const svgrPlugin = require("vite-plugin-svgr");
 
@@ -20,6 +21,24 @@ module.exports = {
     return {
       ...config,
       plugins: [...config.plugins, tsconfigPaths.default(), svgrPlugin()],
-    };
+      resolve: {
+        alias: {
+          '@offer-ui/components/*': path.resolve(__dirname, "../../react/src/components/*"),
+          '@offer-ui/components': path.resolve(__dirname, "../../react/src/components"),
+          '@offer-ui/constants/*': path.resolve(__dirname, "../../react/src/constants/*"),
+          '@offer-ui/constants': path.resolve(__dirname, "../../react/src/constants"),
+          '@offer-ui/hooks/*': path.resolve(__dirname, "../../react/src/hooks/*"),
+          '@offer-ui/hooks': path.resolve(__dirname, "../../react/src/hooks"),
+          '@offer-ui/styles/*': path.resolve(__dirname, "../../react/src/styles/*"),
+          '@offer-ui/styles': path.resolve(__dirname, "../../react/src/styles"),
+          "@offer-ui/themes/*": path.resolve(__dirname, "../../react/src/styles/themes/*"),
+          "@offer-ui/themes": path.resolve(__dirname, "../../react/src/styles/themes"),
+          "@offer-ui/types/*": path.resolve(__dirname, "../../react/src/types/*"),
+          "@offer-ui/types": path.resolve(__dirname, "../../react/src/types"),
+          "@offer-ui/utils/*": path.resolve(__dirname, "../../react/src/utils/*"),
+          "@offer-ui/utils": path.resolve(__dirname, "../../react/src/utils")
+        }
+      }
+    }
   },
 };

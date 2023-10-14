c66dcc5cf84b4f1095f5ef3cbffc00bc3624325c
shinhyojeong <hyojeonge0@naver.com>
2023-10-14 21:33:33 +0900

    design: 사용처에서 지정한 사이즈를 유지하도록 수정

--- packages/react/src/components/Avatar/index.tsx
+++ packages/react/src/components/Avatar/index.tsx
@@ -1,3 +1,5 @@
+import type { SerializedStyles } from '@emotion/react'
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import { Icon } from '@offer-ui/components/Icon'
 import { Image } from '@offer-ui/components/Image'
@@ -72,9 +74,13 @@ const StyledBlankAvatarWrapper = styled.div<StyledBlankAvatarWrapperProps>`
   justify-content: center;
   align-items: center;
   border-radius: 100%;
-  ${({ theme, size }): string => `
+  ${({ theme, size }): SerializedStyles => css`
     width: ${AVATAR_WRAPPER_SIZE[size]};
     height: ${AVATAR_WRAPPER_SIZE[size]};
+    min-width: ${AVATAR_WRAPPER_SIZE[size]};
+    min-height: ${AVATAR_WRAPPER_SIZE[size]};
+    max-width: ${AVATAR_WRAPPER_SIZE[size]};
+    max-height: ${AVATAR_WRAPPER_SIZE[size]};
     background-color: ${theme.colors.grayScale05};
   `}
 `

004e60bc1924dc623bcd233ebf436853ae4bdf00
sonsurim <surim014@naver.com>
2023-10-31 01:08:38 +0900

    feat: implement offer-design-system playground

--- apps/ods-playground/src/main.tsx
+++ apps/ods-playground/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { OfferStyleProvider } from '@offer-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <OfferStyleProvider>
     <App />
-  </React.StrictMode>,
+  </OfferStyleProvider>,
 )

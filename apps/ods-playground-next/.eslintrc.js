5d6f35b82744d450c3d7ff759bd3dcf70613e7c5
sonsurim <surim014@naver.com>
2023-11-12 01:10:28 +0900

    fix next playground some errors

---
+++ apps/ods-playground-next/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'next',
+    'next/core-web-vitals',
+  ]
+}
+
